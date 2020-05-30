4b8ce779119f5cccd1e0b3e9d7ef0981dcbdd05a
gharris999 <wgordonharris@gmail.com>
2020-05-29 22:36:18 -0600

    20200529.223616 commit

--- scripts/lcwa-speed-update.sh
+++ scripts/lcwa-speed-update.sh
@@ -2,7 +2,7 @@
 # lcwa-speed-update.sh -- script to update lcwa-speed git repo and restart service..
 # Version Control for this script
 {
-SCRIPT_VERSION=20200529.223409
+SCRIPT_VERSION=20200529.223616
 
 INST_NAME='lcwa-speed'
 
