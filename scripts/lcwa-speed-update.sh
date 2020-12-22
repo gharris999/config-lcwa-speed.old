3bca82344f07af9de4009f51dcb9f07fe326f18d
gharris999 <wgordonharris@gmail.com>
2020-12-22 13:17:55 -0700

    20201222.131747 commit

--- scripts/lcwa-speed-update.sh
+++ scripts/lcwa-speed-update.sh
@@ -2,7 +2,7 @@
 # lcwa-speed-update.sh -- script to update lcwa-speed git repo and restart service..
 # Version Control for this script
 
-SCRIPT_VERSION=20201222.125732
+SCRIPT_VERSION=20201222.131203
 
 INST_NAME='lcwa-speed'
 
@@ -384,7 +384,7 @@ utility_scripts_install(){
 service_update_check(){
 	local LLOCAL_REPO="$1"
 	local LINSTALL_XML="${LLOCAL_REPO}/install.xml"
-	local LREPO_VERSION=20201222.125732
+	local LREPO_VERSION=20201222.131203
 	local LREPO_EPOCH=
 	local LLCWA_EPOCH=
 	
@@ -396,7 +396,7 @@ service_update_check(){
 	log_msg "Checking ${LLOCAL_REPO}/install.xml to see if an update of the ${INST_NAME} service is required."
 	
 	#~ <version>20200511.232252</version>
-	LREPO_VERSION=20201222.125732
+	LREPO_VERSION=20201222.131203
 	
 	if [ $DEBUG -gt 0 ]; then
 		LREPO_EPOCH="$(echo "$LREPO_VERSION" | sed -e 's/\./ /g' | sed -e 's/\([0-9]\{8\}\) \([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/\1 \2:\3:\4/')"
@@ -409,9 +409,9 @@ service_update_check(){
 		log_msg "'  Repo: [${LREPO_EPOCH}] $(date_epoch_to_iso8601  ${LREPO_EPOCH})"
 
 		if [ $LLCWA_EPOCH -lt $LREPO_EPOCH ]; then
-			log_msg "Running ${SCRIPT} version is older than repo ${LLOCAL_REPO} by $(displaytime $(echo "${LREPO_EPOCH} - ${LLCWA_EPOCH}" | bc))."
+			log_msg "Running ${SCRIPT} version is older than repo ${LINSTALL_XML} by $(displaytime $(echo "${LREPO_EPOCH} - ${LLCWA_EPOCH}" | bc))."
 		else
-			log_msg "Running ${SCRIPT} version is newer than repo ${LLOCAL_REPO} by $(displaytime $(echo "${LLCWA_EPOCH} - ${LREPO_EPOCH}" | bc))." 
+			log_msg "Running ${SCRIPT} version is newer than repo ${LINSTALL_XML} by $(displaytime $(echo "${LLCWA_EPOCH} - ${LREPO_EPOCH}" | bc))." 
 		fi
 	fi
 	
